d6647fa00d3d1f7f6d608a1263de6a9fe47019a8
Taneros <fricentaurus@GMAIL.COM>
2021-12-02 21:37:26 +0300

    init: install webpack & copy files

---
+++ migration-newip-to-ts/news-JS/src/index.js
@@ -0,0 +1,15 @@
+import App from './components/app/app';
+import './global.css';
+
+const app = new App();
+app.start();
+
+// webpack HMR
+if (module.hot) {
+  module.hot.accept();
+  module.hot.addStatusHandler((status) => {
+    if (status === 'apply') {
+      console.clear();
+    }
+  });
+}
