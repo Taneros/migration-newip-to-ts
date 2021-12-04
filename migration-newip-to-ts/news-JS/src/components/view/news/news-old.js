6b29db62e79b708c96a812064cbea0da08552de9
Taneros <fricentaurus@GMAIL.COM>
2021-12-04 17:39:47 +0300

    refactor: change news ts

--- migration-newip-to-ts/news-JS/src/components/view/news/news-old.js
+++ migration-newip-to-ts/news-JS/src/components/view/news/news-old.js
@@ -2,12 +2,20 @@ import './news.css';
 
 class News {
   draw(data) {
+
+    console.log('data:', data)
+
     const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+    console.log('news:', news)
 
     const fragment = document.createDocumentFragment();
     const newsItemTemp = document.querySelector('#newsItemTemp');
 
     news.forEach((item, idx) => {
+
+      console.log('item: ', item, 'idx: ', idx)
+
       const newsClone = newsItemTemp.content.cloneNode(true);
 
       if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
---
+++ migration-newip-to-ts/news-JS/src/components/view/news/news.ts
@@ -0,0 +1,64 @@
+import './news.css';
+
+interface IData {
+  source: {
+    name: string,
+    id: string
+  },
+  title: string,
+  url: string,
+  description: string,
+  author: string,
+  urlToImage: string,
+  publishedAt: string
+}
+
+class News {
+  draw(data: Array<IData>) {
+    const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+
+    const fragment = document.createDocumentFragment();
+    const newsItemTemp = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
+
+    news.forEach((item, idx) => {
+      const newsClone = newsItemTemp.content.cloneNode(true) as DocumentFragment;
+      const newsItem = newsClone.querySelector('.news__item') as HTMLElement;
+      if (idx % 2) newsItem.classList.add('alt');
+      const newsMetaPhoto = newsClone.querySelector('.news__meta-photo') as HTMLElement;
+      newsMetaPhoto.style.backgroundImage = `url(${
+        item.urlToImage || 'img/news_placeholder.jpg'
+      })`;
+
+      const newsMetaAuthor = newsClone.querySelector('.news__meta-author') as HTMLElement;
+      newsMetaAuthor.textContent = item.author || item.source.name;
+
+      const newsMetaDate = newsClone.querySelector('.news__meta-date') as HTMLElement;
+
+      newsMetaDate.textContent = item.publishedAt
+        .slice(0, 10)
+        .split('-')
+        .reverse()
+        .join('-');
+
+      const newsDescriptionTitle = newsClone.querySelector('.news__description-title') as HTMLElement;
+      newsDescriptionTitle.textContent = item.title;
+      
+      const newsDescriptionSource = newsClone.querySelector('.news__description-source') as HTMLElement;
+      newsDescriptionSource.textContent = item.source.name;
+
+      const newsDescriptionContent = newsClone.querySelector('.news__description-content') as HTMLElement;
+      newsDescriptionContent.textContent = item.description;
+
+      const newsReadMoreEla = newsClone.querySelector('.news__read-more a') as HTMLElement;
+      newsReadMoreEla.setAttribute('href', item.url);
+      fragment.append(newsClone);
+    });
+
+    const newsEl = document.querySelector('.news') as HTMLElement;
+    newsEl.innerHTML = '';
+
+    newsEl.appendChild(fragment);
+  }
+}
+
+export default News;
