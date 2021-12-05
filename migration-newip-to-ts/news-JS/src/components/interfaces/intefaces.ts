2f523ce041ccd26e74e94aee672209536a094ab3
Taneros <fricentaurus@GMAIL.COM>
2021-12-05 23:55:37 +0300

    feat: add interfaces file

---
+++ migration-newip-to-ts/news-JS/src/components/interfaces/intefaces.ts
@@ -0,0 +1,59 @@
+interface IData {
+  status: string,
+  sources: [
+      {
+          id: string,
+          name: string,
+          description: string,
+          url: string,
+          category: string,
+          language: string,
+          country: string
+      },]
+}
+
+
+interface IDataArt {
+  status: string,
+  totalResults: number,
+  articles: [
+      {
+          source: {
+              id: string,
+              name: string
+          },
+          author: string,
+          title: string,
+          description: string,
+          url: string,
+          urlToImage: string,
+          publishedAt: string,
+          content: string
+      }]
+}
+
+interface Options {
+  [key: string] : string;
+  // sources : string;
+}
+
+interface Data {
+    status?: string,
+    sources?: Array<Options>
+}
+
+interface INews {
+  source: {
+    name: string,
+    id: string
+  },
+  title: string,
+  url: string,
+  description: string,
+  author: string,
+  urlToImage: string,
+  publishedAt: string
+}
+
+
+export {IData, IDataArt, Options, Data, INews}
