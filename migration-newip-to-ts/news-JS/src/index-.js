a27b7696adb1b885842125c6b328ab432e726ee6
Taneros <fricentaurus@GMAIL.COM>
2021-12-07 02:38:53 +0300

    refactor: move index js

--- migration-newip-to-ts/news-JS/src/index-.js
+++
@@ -1,9 +0,0 @@
-// webpack HMR
-// if (module.hot) {
-//   module.hot.accept();
-//   module.hot.addStatusHandler((status) => {
-//     if (status === 'apply') {
-//       console.clear();
-//     }
-//   });
-// }
