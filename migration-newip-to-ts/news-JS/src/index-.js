a1b76a3b13cbc7642ab8513ab1a0fc4b739fce05
Taneros <fricentaurus@GMAIL.COM>
2021-12-06 21:06:49 +0300

    refactor: index html js

---
+++ migration-newip-to-ts/news-JS/src/index-.js
@@ -0,0 +1,9 @@
+// webpack HMR
+// if (module.hot) {
+//   module.hot.accept();
+//   module.hot.addStatusHandler((status) => {
+//     if (status === 'apply') {
+//       console.clear();
+//     }
+//   });
+// }
