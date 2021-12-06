3f24b0320df518f21b0d3783eba22ab9a4588468
Taneros <fricentaurus@GMAIL.COM>
2021-12-06 21:08:07 +0300

    fix: change API

--- migration-newip-to-ts/news-JS/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/news-JS/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import {Loader} from './loader';
 
 export class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: 'cb6ef4c8ae3d446b81779e64da70b5e3',
     });
   }
